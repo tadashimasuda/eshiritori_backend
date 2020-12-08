54585eb8291a34d06af911b51045edc0bb34789c
tadashimasuda <tmasu4260@gmail.com>
2020-12-08 15:08:19 +0900

    get callbackurl

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,15 @@
+<?php
+// namespace App\Http\Controllers\Auth;
+namespace App\Http\Controllers;
+use App\Http\Controllers\Controller;
+// use Laravel\Socialite\Facades\Socialite;
+use Socialite;
+
+class AuthController extends Controller
+{
+    public function redirectToProvider()
+    {   
+        $url = Socialite::driver('twitter')->redirect()->getTargetUrl();
+        return response()->json(['redirect_url' => $url]);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,18 +1,9 @@
 <?php
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+Route::get('/oauth/twitter/redirect','AuthController@redirectToProvider')->middleware('session');
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+
+
