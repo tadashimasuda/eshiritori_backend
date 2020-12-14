0af57ccad6e5be25fd9e4d02daa4916e74e075c3
tadashimasuda <tmasu4260@gmail.com>
2020-12-14 21:36:35 +0900

    user show api/users/{id}

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,6 +4,8 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Http\Requests\UpdateUserRequest; 
 use App\Http\Resources\User as UserResourse;
+use App\Http\Resources\UserShow as UserShowResourse;
+
 use Socialite;
 use App\User;
 
@@ -60,5 +62,11 @@ class AuthController extends Controller
     {
         $user= User::latestFirst()->paginate(10);
         return UserResourse::collection($user);
+    }
+
+    public function show(Request $request)
+    {
+        $user = User::find($request->id);
+        return new UserShowResourse($user);
     }
 }
