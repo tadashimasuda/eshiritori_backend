e55f58860e989aeadac220f66c71001f999ab620
tadashimasuda <tmasu4260@gmail.com>
2020-12-18 15:37:40 +0900

    table resource

--- app/Table.php
+++ app/Table.php
@@ -19,6 +19,6 @@ class Table extends Model
 		return $this->belongsTo(User::class,'id');
     }
     public function post() {
-		return $this->hasMany(Post::class);
-	}
+    return $this->hasMany(Post::class);
+    }
 }
